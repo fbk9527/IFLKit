98958dd253d7331f1c6d4182b234bcff4e0d1f9d
fbk9527 <fbk9527@rit.edu>
2014-09-17 22:58:01 -0400

    Initial Commit

---
+++ InterfaceliftKit/InterfaceliftKit.h
@@ -0,0 +1,13 @@
+//
+//  InterfaceliftKit.h
+//  InterfaceliftKit
+//
+//  Created by Freddy kelch on 9/17/14.
+//  Copyright (c) 2014 Frederick Kelch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InterfaceliftKit : NSObject
+
+@end
---
+++ InterfaceliftKit/InterfaceliftKit.m
@@ -0,0 +1,13 @@
+//
+//  InterfaceliftKit.m
+//  InterfaceliftKit
+//
+//  Created by Freddy kelch on 9/17/14.
+//  Copyright (c) 2014 Frederick Kelch. All rights reserved.
+//
+
+#import "InterfaceliftKit.h"
+
+@implementation InterfaceliftKit
+
+@end
