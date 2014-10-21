bad53929d1f152b2fb3a209f6d27e7d1bff745ec
Fred Kelch <fbk9527@rit.edu>
2014-10-21 15:35:34 -0400

    standardized comments

--- InterfaceliftKit/IFLRequest.h
+++ InterfaceliftKit/IFLRequest.h
@@ -41,8 +41,10 @@ typedef void (^ResumeDownload)(NSURLSession* session,
 
 
 
-/** The @code IFLRequest @endcode class is an abstract class you subclass to encapsulate code and data associated with a request.
-    Subclass should override the following methods
+/*!
+ * @class IFLRequest
+ *
+ * @discussion IFLRequest is the paretn class for all request managed by the IFLClient.
  */
 @interface IFLRequest : NSOperation <NSURLSessionTaskDelegate, NSURLSessionDownloadDelegate>
 
@@ -77,9 +79,12 @@ typedef void (^ResumeDownload)(NSURLSession* session,
 
 
 
+//==========================================================================================================================
+// Options
+//==========================================================================================================================
 
-
-/** Control the construction of the request URL.
+/*!
+ * @brief The options to be set for the specific request.
  */
 @property(assign,nonatomic) IFLURLOption options;
 
@@ -90,31 +95,55 @@ typedef void (^ResumeDownload)(NSURLSession* session,
 // Blocks
 //==========================================================================================================================
 
-
-
-/** The completion blocked that is called if the request was successfull.
- @warning Be mindful of retain cycles if your enclosing queue is strong referenced!
+/*!
+ * @brief Called when the IFLRequest completes successfully
+ *
+ *
+ * @code [req setSuccessBlock:^(NSData* data,NSURLResponse* resp, NSURLError*error){
+ *
+ * }];
+ * @endcode
+ *
  */
 @property(copy,nonatomic) IFLCallBack successBlock;
 
 
-/** The completion blocked that is called if the request was failed.
- @warning Be mindful of retain cycles if your enclosing queue is strong referenced!
+/*!
+ * @brief Called when the IFLRequest failes to complete successfully
+ *
+ *
+ * @code [req setSuccessBlock:^(NSData* data,NSURLResponse* resp, NSURLError*error){
+ *
+ * }];
+ * @endcode
+ *
  */
 @property(copy,nonatomic) IFLCallBack failureBlock;
 
 
-/** The status block used to inform when data is downloaded.
+/*!
+ * @brief Called throughout the download process, informing the block fo the download progress.
+ *
  */
 @property(assign,nonatomic) BytesWritten statusBlock;
 
 
-/** The block called when the download is resumed.
+/*!
+ * @brief Called when the IFLRequest resumes downloading its current payload.
+ *
  */
 @property(assign,nonatomic) ResumeDownload resumeBlock;
 
 
-/**
+/*!
+ * @brief The NSURLSessionDownloadTask to be performed on the background thread
+ *
+ * @discussion A client programmer should not need to set this property unless
+ * the client doesn't want the IFLClient creating/managing their network request to Interfacelift.
+ * However, keep in mind that if you set this property, the client will not create one for you.
+ * Additionally, you will need to set the authentication headers yourself since this is handled for 
+ * you by the IFLClient.
+ *
  */
 @property(strong,nonatomic) NSURLSessionDownloadTask* networkTask;
 
@@ -122,23 +151,105 @@ typedef void (^ResumeDownload)(NSURLSession* session,
 
 
 
+//==========================================================================================================================
+// Request Parameters
+//==========================================================================================================================
+
+/*!
+ * @brief Inform the request how it should order the results.
+ *
+ * @discussion The sort_by property is not used by all IFLRequest.
+ * Currently, V1 of the interfacelift API only accepts this on *IFLFavoriteRequest* and *IFLWallpapers* request.
+ * This parameter can be set on any request, but its only utalized on those listed.
+ */
+@property(strong,nonatomic) NSString* sort_by;
+
+
+/*!
+ * @brief The ID of the requested object.
+ */
+@property(strong,nonatomic) NSNumber* iflId;
+
+
+/*!
+ * @brief Limit the number of results returned by the request.
+ */
+@property(strong,nonatomic) NSNumber* limit;
+
+
+/*!
+ * @brief The order in which results should be returned (ASC or DESC)
+ */
+@property(strong,nonatomic) NSString* sort_order;
+
+
+/*!
+ * @brief The offset of the results to start.
+ */
+@property(strong,nonatomic) NSNumber* iflstart;
+
+
+/*!
+ * @brief The ID of a specific user.
+ */
+@property(strong,nonatomic) NSNumber* user_id;
+
+
+/*!
+ * @brief The ID of a specific wallpaper.
+ */
+@property(strong,nonatomic) NSNumber* wallpaper_id;
+
+
+/*!
+ * @brief The string representation of the device screensize.
+ */
+@property(strong,nonatomic) NSString* resolution;
+
+
+/*!
+ * @brief The ID of a specific submission
+ */
+@property(strong,nonatomic) NSNumber* submission_id;
+
+
+/*!
+ * @brief The search string to search tags in the for the API.
+ */
+@property(strong,nonatomic) NSString* search;
+
+
+/*!
+ * @brief The type of tag.
+ */
+@property(strong,nonatomic) NSString* type;
+
+
+/*!
+ * @brief The ID of a specific tag.
+ */
+@property(strong,nonatomic) NSNumber* tag_id;
+
+
+/*!
+ * @brief The direction to sort
+ */
+@property(strong,nonatomic) NSString* direction;
+
+
+/*!
+ * @brief The UNIX timestamp of the wallpaper submission
+ */
+@property(strong,nonatomic) NSNumber* timestamp;
+
+
+//==========================================================================================================================
+// Class constents
+//==========================================================================================================================
+
 extern NSString* kIFLRequestSortByDate;
 extern NSString* kIFLRequestSortById;
 extern NSString* kIFLRequestSortByFavorites;
 extern NSString* kIFLRequestSortByDownloads;
 extern NSString* kIFLRequestSortByComments;
-@property(strong,nonatomic) NSString* sort_by;
-@property(strong,nonatomic) NSNumber* iflId;
-@property(strong,nonatomic) NSNumber* limit;
-@property(strong,nonatomic) NSString* sort_order;
-@property(strong,nonatomic) NSNumber* iflstart;
-@property(strong,nonatomic) NSNumber* user_id;
-@property(strong,nonatomic) NSNumber* wallpaper_id;
-@property(strong,nonatomic) NSString* resolution;
-@property(strong,nonatomic) NSNumber* submission_id;
-@property(strong,nonatomic) NSString* search;
-@property(strong,nonatomic) NSString* type;
-@property(strong,nonatomic) NSNumber* tag_id;
-@property(strong,nonatomic) NSString* direction;
-@property(strong,nonatomic) NSNumber* timestamp;
 @end
